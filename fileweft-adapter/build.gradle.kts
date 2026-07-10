193d8a6caa918487d3d7aa939be94f5875edd908
J.K. <dev@cns.red>
2026-07-10 18:13:55 +0800

    feat[adapter]: 实现本地存储适配器

--- fileweft-adapter/build.gradle.kts
+++ fileweft-adapter/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 dependencies {
     api(project(":fileweft-spi"))
     implementation(project(":fileweft-core"))
+    testImplementation(project(":fileweft-testkit"))
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotlin.test)
     testRuntimeOnly(libs.junit.platform.launcher)
 }
